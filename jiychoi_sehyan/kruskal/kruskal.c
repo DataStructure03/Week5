658c846480d5ac94ff3d6c51236f09109f552871
Sehyeon An <ash@Sehyeonui-MacBookAir.local>
2021-11-13 18:45:23 +0900

    sehyan_modi

--- jiychoi_sehyan/kruskal/kruskal.c
+++ jiychoi_sehyan/kruskal/kruskal.c
@@ -21,12 +21,37 @@ int main()
 	displayArrayGraph(graph);
 	printf("=====\n");
 
+	int **infoList;
+
+	infoList = (int **)calloc(MAX, sizeof(int *));
+	for (int i = 0; i < MAX; i++) {
+		infoList[i] = (int *)calloc(sizeof(int), 3);
+	}
+
+	arrHeap *heap_sort;
+
+	heap_sort = createMinHeap();
 	for (int i = 0; i < graph->maxVertexCount; i++)
 	{
 		if (graph->pVertex[i] == TRUE) {
-			
+			for (int j = 0; j < graph->maxVertexCount; j++) {
+				if (graph->ppAdjEdge[i][j]) {
+					infoList[i][0] = graph->ppAdjEdge[i][j];
+					infoList[i][1] = i;
+					infoList[i][2] = j;
+					// break;
+					element ele;
+					ele.data = infoList[i][0];
+					insertHeap(heap_sort, ele);
+				}
+			}
 		}
 	}
+	for (int i = 1 ; i <= heap_sort->currentSize ; i++)
+        printf("%d ", heap_sort->heap[i].data);
+	// for (int i = 0; i < MAX; i++) {
+	// 	printf("{%d %d %d}\n", infoList[i][0], infoList[i][1], infoList[i][2]);
+	// }
 
 	deleteArrayGraph(graph);
 	// system("leaks a.out");
