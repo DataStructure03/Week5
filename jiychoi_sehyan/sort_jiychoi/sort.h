bd27ae672d691b1547045c2767f91c80ae68b1eb
chichoon <chichoon.choi@gmail.com>
2021-11-20 16:05:36 +0900

    [~1123] add: jiychoi sort

---
+++ jiychoi_sehyan/sort_jiychoi/sort.h
@@ -0,0 +1,19 @@
+#ifndef SORT_H
+# define SORT_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+# include <string.h>
+
+void bubble_sort(int* a, int N);
+void insert_sort(int* a, int N);
+void heap_sort(int* a, int N);
+
+#define MAX 100
+#define INT_MAX 2147483647
+
+
+
+
+#endif
